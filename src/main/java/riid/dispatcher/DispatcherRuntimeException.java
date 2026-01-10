ec328840e90f3dd9c7ac137290ab3097d4e5ed01
Usatov Pavel <pvusatov@edu.hse.ru>
2026-01-10 13:12:40 +0300

    Dispatcher-Runtime integration
     DispatcherRuntimeIntegrator: получает FetchResult от RequestDispatcher, валидирует digest/mediaType/файл, и импортирует в переданный RuntimeAdapter
    DispatcherRuntimeException

---
+++ src/main/java/riid/dispatcher/DispatcherRuntimeException.java
@@ -0,0 +1,16 @@
+package riid.dispatcher;
+
+/**
+ * Wraps failures that happen when handing a fetched image over to a runtime adapter.
+ */
+public class DispatcherRuntimeException extends RuntimeException {
+    public DispatcherRuntimeException(String message) {
+        super(message);
+    }
+
+    public DispatcherRuntimeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
+
