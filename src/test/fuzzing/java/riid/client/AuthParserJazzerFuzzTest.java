65f9d8132733b47507034caee0dd8b42e9bab1f2
Usatov Pavel <dbpfynbqcrfzbvgthbz@gmail.com>
2026-02-03 01:06:04 +0300

    Fuzzing basic tests
    Add auth/imageDigest/manifest fuzzing tests
    ImageLoading with mocking: validate that random images not downloading, thrown appError
    
    Use jazzer for coverage, add dictionary to resources
    Fuzzing tests under tag Stress

---
+++ src/test/fuzzing/java/riid/client/AuthParserJazzerFuzzTest.java
@@ -0,0 +1,16 @@
+package riid.client;
+
+import com.code_intelligence.jazzer.api.FuzzedDataProvider;
+import com.code_intelligence.jazzer.junit.FuzzTest;
+import org.junit.jupiter.api.Tag;
+import riid.client.core.model.auth.AuthParser;
+
+@Tag("stress")
+class AuthParserJazzerFuzzTest {
+
+    @FuzzTest
+    void fuzzAuthHeader(FuzzedDataProvider data) {
+        String header = data.consumeString(512);
+        AuthParser.parse(header);
+    }
+}
