af58c19dc1d54d1bf63621114d3a3db7cfa89493
Usatov Pavel <dbpfynbqcrfzbvgthbz@gmail.com>
2026-05-01 22:26:01 +0300

    Revert "temp logs in daemon api"
    This reverts commit 09414035262932dc2e4fed372408bee297d7450f.

--- src/main/java/riid/app/daemon/handler/MetricsHttpHandler.java
+++ src/main/java/riid/app/daemon/handler/MetricsHttpHandler.java
@@ -12,11 +12,8 @@ import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Response;
 import org.eclipse.jetty.util.BufferUtil;
 import org.eclipse.jetty.util.Callback;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public final class MetricsHttpHandler extends Handler.Abstract {
-    private static final Logger LOGGER = LoggerFactory.getLogger(MetricsHttpHandler.class);
     private static final String METRICS_PATH = "/metrics";
 
     private final String metricsConnectorName;
@@ -29,35 +26,21 @@ public final class MetricsHttpHandler extends Handler.Abstract {
 
     @Override
     public boolean handle(Request request, Response response, Callback callback) {
-        String connectorName = request.getConnectionMetaData().getConnector().getName();
-        String path = request.getHttpURI().getPath();
-        String method = request.getMethod();
-        LOGGER.info("metrics.handle.enter connector={} expectedConnector={} path={} method={} thread={}",
-                connectorName, metricsConnectorName, path, method, Thread.currentThread().getName());
-        if (!metricsConnectorName.equals(connectorName)) {
-            LOGGER.warn("metrics.handle.skip connector mismatch connector={} expected={} path={} method={}",
-                    connectorName, metricsConnectorName, path, method);
+        if (!metricsConnectorName.equals(request.getConnectionMetaData().getConnector().getName())) {
             return false;
         }
-        if (!METRICS_PATH.equals(path)) {
-            LOGGER.warn("metrics.handle.skip path mismatch connector={} path={} expectedPath={}",
-                    connectorName, path, METRICS_PATH);
+        if (!METRICS_PATH.equals(request.getHttpURI().getPath())) {
             return false;
         }
-        if (!HttpMethod.GET.is(method)) {
-            LOGGER.warn("metrics.handle.method_not_allowed connector={} path={} method={}",
-                    connectorName, path, method);
+        if (!HttpMethod.GET.is(request.getMethod())) {
             Response.writeError(request, response, callback, HttpStatus.METHOD_NOT_ALLOWED_405);
             return true;
         }
 
-        LOGGER.info("metrics.handle.scrape.start connector={} path={}", connectorName, path);
         response.setStatus(HttpStatus.OK_200);
         response.getHeaders().put(HttpHeader.CONTENT_TYPE, "text/plain; version=0.0.4; charset=utf-8");
         byte[] body = prometheusRegistry.scrape().getBytes(StandardCharsets.UTF_8);
-        LOGGER.info("metrics.handle.scrape.done connector={} path={} bytes={}", connectorName, path, body.length);
         response.write(true, BufferUtil.toBuffer(body), callback);
-        LOGGER.info("metrics.handle.exit connector={} path={} status={}", connectorName, path, HttpStatus.OK_200);
         return true;
     }
 }
